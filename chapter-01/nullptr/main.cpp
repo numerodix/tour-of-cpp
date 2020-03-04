6b43af241972941762e101041062788bf2dea630
Martin Matusiak <numerodix@gmail.com>
2020-03-04 23:23:31 +1100

    wip: chapter-01

---
+++ chapter-01/nullptr/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int count_x(char* p, char x) {
+    if (p == nullptr) return 0;
+    int count = 0;
+    for (; p != nullptr; ++p)  // why would p ever become equal to nullptr tho??
+        if (*p == x)
+            ++ count;
+    return count;
+}
+
+int count_x2(char* p, char x) {
+    int count = 0;
+    while (p) {                 // same question here!
+        if (*p == x)
+            ++count;
+        ++p;
+    }
+    return count;
+}
+
+int main()
+{
+    char name[] = "James";
+    std::cout << name << std::endl;
+    std::cout << count_x(name, 'a') << std::endl;
+    return 0;
+}
